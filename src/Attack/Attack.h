9eea287df6451c349315c83b31147360aba06fc9
Keita Marumoto <marumoto.keita@gmail.com>
2015-11-26 17:42:14 +0900

    AIとAttack更新
    しました

--- src/Attack/Attack.h
+++ src/Attack/Attack.h
@@ -1,11 +1,23 @@
 #pragma once
 #include "../basicrelated/Common.h"
 
-class Attack {
+class Attack 
+{
 public:
 	Attack();
+	bool isAttack();
 	void Bomb();
-	void RocketLancher();
+	void Launcher();
+	Vec2f getAttackStartPos();
+	void setAttackStartPos(Vec2f);
+	Vec2f getBombPos();
+	void setBombPos(Vec2f);
+	Vec2f getLauncherPos();
+	void setLauncherPos(Vec2f);
+
 private:
-	Vec2f attack_start_pos;
+	Vec2f start_pos;                    //攻撃開始地点
+	Vec2f bomb_pos;                     //Bomb攻撃開始点
+	Vec2f launcher_pos;                 //launcher攻撃開始点
+	bool isAttacking;                   //攻撃中
 };
